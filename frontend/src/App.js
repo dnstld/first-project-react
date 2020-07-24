87be2c6180561fa7d35749d7fddf03ee73ad85d5
Denis Toledo <contato@denistoledo.com.br>
2020-07-24 10:50:41 -0300

    props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,17 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   )
 }
